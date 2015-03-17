3a32e00cf73e5cc9b2a2c1e12d2440900372dacb
Tim Hawes <tim.hawes@inin.com>
2015-03-17 11:39:23 -0400

    progress

--- Chapter01.scm
+++ Chapter01.scm
@@ -1,4 +1,3 @@
-;; #lang planet neil/sicp
 
 (define (square x) (* x x))
 
@@ -10,3 +9,26 @@
 (define (f a)
   (sum-of-squares (+ a 1) (* a 2)))
 (f 5)
+
+
+(define (abs x)
+  (cond ((> x 0) x)
+        ((= x 0) 0)
+        ((< x 0) (- x))))
+10
+
+(+ 5 3 4)
+
+(- 9 1)
+
+(/ (+ 5 (- 2 3 (+ 6 1/3))) (* 3 (- 6 2) (- 2 7)))
+
+(define exc3 (a b c)
+    (if (> a b)
+      (if (> b c)
+          (+ (* a a) (* b b))
+          (+ (* a a) (* c c)))
+      (if (> b c)
+          (if (> a c)
+              (+ (* a a) (* b b))
+              (+ (* b b) (* c c))))))
